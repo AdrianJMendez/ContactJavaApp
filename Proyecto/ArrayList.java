ce7db0c06f50ba17894b79491f1bac1563a668cb
AdrianJMendez <esusjadrian@gmail.com>
2023-04-05 20:11:34 -0600

    xd

--- Proyecto/ArrayList.java
+++ Proyecto/ArrayList.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class ArrayList implements List {
 
     Contacto[] ListaContacto = new Contacto();
-
+//ahhhhhhhhhhhhhhhhhhh
 
     public Contacto[] getListaContacto() {
         return ListaContacto;
