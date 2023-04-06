54b90cd5ebf2493dea4dee2b02c3177dce6c78c8
Cronocar <eucedao662@gmail.com>
2023-04-05 20:08:07 -0600

    xd

--- Proyecto/Controlador.java
+++ Proyecto/Controlador.java
@@ -3,3 +3,16 @@ package Proyecto;
 public class Controlador
 {
 }
+//metodo agregar contacto
+    public static void agregarContacto(ArrayList<Contacto> lista, Contacto nuevoContacto) {
+
+        if(lista==null){
+
+                 System.out.println("la Lista esta vacia ");
+
+        }else{
+                lista.add(nuevoContacto);
+
+        }
+
+        }
