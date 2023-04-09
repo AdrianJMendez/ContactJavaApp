10efbbb452f3b6a74399e36723e504611c69de4f
AdrianJMendez <esusjadrian@gmail.com>
2023-04-08 21:30:32 -0600

    Creacion del metodo para agregar arraylist en la linkedlist

--- Proyecto/ContactList.java
+++ Proyecto/ContactList.java
@@ -11,11 +11,17 @@ public class ContactList {
 
     LinkedList<ArrayList<Contacto>> listaContactos = new LinkedList<ArrayList<Contacto>>();
 
-    //Sublistas
+    //Sublistas de la A a Z
+    public void SublistaLetras() {
         for (int i = 0; i < 27; i++) {
-            ArrayList<Contacto> nvaLista= new ArrayList<Contacto>();
-            listaContactos.add(nvaLista);
+            this.listaContactos.add(new ArrayList<Contacto>());
+        }
     }
+    //Sublista de nombres con caracteres
+    public void Sublista() {
+            this.listaContactos.add(new ArrayList<Contacto>());
+    }
+
 
     /**
      * Agrega un contacto a la lista de contactos.
@@ -23,9 +29,9 @@ public class ContactList {
      * @param nvoContacto El objeto Contacto que se desea agregar.
      */
     protected void Add(Contacto nvoContacto){
-         char PrimeraLetra = nvoContacto.getNombre().charAt(0); //Revisa la primera letra del Nombre del contacto
-            for(int i = 0; i < 27; i++){
-                char letra = 'A';
+        char PrimeraLetra = nvoContacto.getNombre().charAt(0); //Revisa la primera letra del Nombre del contacto
+        char letra = 'A';
+        for(int i = 0; i < 27; i++){
                     if(Character.toLowerCase(letra)== Character.toLowerCase(PrimeraLetra)){
                             listaContactos.get(i).add(nvoContacto);
 
@@ -42,8 +48,8 @@ public class ContactList {
      */
         protected void Remove(String Nombre){
             char PrimeraLetra = Nombre.charAt(0); //Revisa la primera letra del Nombre del contacto
+            char letra = 'A';
             for(int i = 0; i < 27; i++){
-                char letra = 'A';
                 if(Character.toLowerCase(letra)== Character.toLowerCase(PrimeraLetra)){
                     ArrayList<Contacto> listaI =  listaContactos.get(i);
                      for(int j = 0; j < listaI.size(); j++){
@@ -68,8 +74,8 @@ public class ContactList {
         protected Contacto Search(String Nombre){
             Contacto nvoContacto = null;
             char PrimeraLetra = Nombre.charAt(0); //Revisa la primera letra del Nombre del contacto
+            char letra = 'A';
             for(int i = 0; i < 27; i++){
-                char letra = 'A';
                 if(Character.toLowerCase(letra)== Character.toLowerCase(PrimeraLetra)){// compara las letras para saber a que sublista pertenece
                     ArrayList<Contacto> listaI =  listaContactos.get(i);
                     for(int j = 0; j < listaI.size(); j++){
---
+++ Proyecto/EjemploFor.java
@@ -0,0 +1,22 @@
+package Proyecto;
+
+public class EjemploFor {
+
+
+        public static void main(String[] args) {
+            // Declarar una variable entera para la suma
+            int suma = 0;
+
+            // Crear un ciclo for que se ejecute 10 veces
+            for (int i = 1; i <= 10; i++) {
+                // Agregar el valor de i a la suma
+                suma += i;
+            }
+
+            // Imprimir el resultado de la suma
+            System.out.println("La suma de los números del 1 al 10 es: " + suma);
+        }
+
+    }
+
+
